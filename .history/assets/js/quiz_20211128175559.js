0875e8755e295abc65c68077e3bf868ec79367db
brenhamp <bpozzi@trinity.edu>
2021-11-28 17:56:34 -0600

    added questions and answers

--- .history/assets/js/quiz_20211128175559.js
+++ .history/assets/js/quiz_20211128175559.js
@@ -5,25 +5,25 @@ var questions = [
         choices: ["a. strings", "b. boolean values", "c. other arrays", "d. all of the above"],
         answer: "d. all of the above"
 
-    },
+    }
 
     {
         question: "String values can be denoted by enclosing them in _____.",
         choices: ["a. square braces", "b. curly braces", "c. quotation marks", "d. parentheses"],
         answer: "c. quotation marks"
-    },
+    }
 
     {
         question: "What is the difference between == and === in JavaScript?",
         choices: ["a. == checks for value equality, === checks for value and type equality", "b. == checks for type equality, === checks for value and type equality", "c. both of them check for value and type equality, and they function the same", "d. none of the above"],
         answer: "a. == checks for value equality, === checks for value and type equality"
-    },
+    }
 
     {
         question: "How do you add a single-line comment in JavaScript?",
         choices: ["a. <!--comment--!>", "b. //comment", "c. /*comment*/", "d. #comment"],
         answer: "b. //comment"
-    },
+    }
 
     {
         question: "Which of the following is an example of a DOM event?",
